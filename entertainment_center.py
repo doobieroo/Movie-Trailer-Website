916e92ce3985eb000bdb86a0eb00324c9ed040a6
doobieroo <robin@doobieroo.com>
2017-08-17 21:33:49 -0700

    Changes suggested by Udacity reviewer

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,45 +1,57 @@
 import media
 import fresh_tomatoes
 
-#Create new instances of class Movie for each movie in the web page
-the_holiday = media.Movie("The Holiday",
-                        "Two women meet their dream man.",
-                        "https://upload.wikimedia.org/wikipedia/en/6/60/Theholidayposter.jpg",
-                        "https://www.youtube.com/watch?v=JGBl5FxOrUQ")
 
+# The Holiday: movie title, storyline, poster art, and YouTube trailer
+the_holiday = media.Movie(
+    "The Holiday",
+    "Two women meet their dream man.",
+    "https://upload.wikimedia.org/wikipedia/en/6/60/Theholidayposter.jpg",
+    "https://www.youtube.com/watch?v=JGBl5FxOrUQ")
 
-idiocracy = media.Movie("Idiocracy",
-                     "The world has gotten dumb, really dumb.",
-                     "https://upload.wikimedia.org/wikipedia/en/6/6b/Idiocracy_movie_poster.jpg",
-                     "https://www.youtube.com/watch?v=ztSngdklb5g")
+# Idiocracy: movie title, storyline, poster art, and YouTube trailer
+idiocracy = media.Movie(
+    "Idiocracy",
+    "The world has gotten dumb, really dumb.",
+    "https://upload.wikimedia.org/wikipedia/en/6/6b/Idiocracy_movie_poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=ztSngdklb5g")
 
+# Dirty Dancing: movie title, storyline, poster art, and YouTube trailer
+dirty_dancing = media.Movie(
+    "Dirty Dancing",
+    "Girl meets inappropriate soul mate - grows up.",
+    "https://upload.wikimedia.org/wikipedia/en/0/00/Dirty_Dancing.jpg",
+    "https://www.youtube.com/watch?v=eIcmQNy9FsM")
 
+# Pretty Woman: movie title, storyline, poster art, and YouTube trailer
+pretty_woman = media.Movie(
+    "Pretty Woman",
+    "Guy meets girl from wrong side of town, love happens.",
+    "https://upload.wikimedia.org/wikipedia/en/b/b6/Pretty_woman_movie.jpg",
+    "https://www.youtube.com/watch?v=Ng2hEIWVqqo")
 
-dirty_dancing = media.Movie("Dirty Dancing",
-                            "Girl meets inappropriate soul mate - grows up.",
-                            "https://upload.wikimedia.org/wikipedia/en/0/00/Dirty_Dancing.jpg",
-                            "https://www.youtube.com/watch?v=eIcmQNy9FsM")
+# Sixteen Candles: movie title, storyline, poster art, and YouTube trailer
+sixteen_candles = media.Movie(
+    "Sixteen Candles",
+    "Awkward teenage romance of the 80's.",
+    "https://upload.wikimedia.org/wikipedia/en/3/34/Sixteen_Candles.jpg",
+    "https://www.youtube.com/watch?v=HGLtBJupFFM")
 
+# American Pie: movie title, storyline, poster art, and YouTube trailer
+american_pie = media.Movie(
+    "American Pie",
+    "Teen sex comedy featuring pie and flutes.",
+    "https://upload.wikimedia.org/wikipedia/en/c/c8/American_Pie1.jpg",
+    "https://www.youtube.com/watch?v=RnWpLaN84uM")
 
-pretty_woman = media.Movie("Pretty Woman",
-                           "Guy meets girl from wrong side of town, love happens.",
-                           "https://upload.wikimedia.org/wikipedia/en/b/b6/Pretty_woman_movie.jpg",
-                           "https://www.youtube.com/watch?v=Ng2hEIWVqqo")
+# create list of all movie objects
+movies = [
+      the_holiday,
+      idiocracy,
+      dirty_dancing,
+      pretty_woman,
+      sixteen_candles,
+      american_pie]
 
-sixteen_candles = media.Movie("Sixteen Candles",
-                              "Awkward teenage romance of the 80's.",
-                              "https://upload.wikimedia.org/wikipedia/en/3/34/Sixteen_Candles.jpg",
-                              "https://www.youtube.com/watch?v=HGLtBJupFFM")
-
-
-american_pie = media.Movie("American Pie",
-                           "Teen sex comedy featuring pie and flutes.",
-                           "https://upload.wikimedia.org/wikipedia/en/c/c8/American_Pie1.jpg",
-                           "https://www.youtube.com/watch?v=RnWpLaN84uM")
-
-#create list of all movie objects
-movies = [the_holiday,idiocracy,dirty_dancing, pretty_woman, sixteen_candles, american_pie]
-
-#call function from fresh_tomatoes.py to create an html file for display
+# call function from fresh_tomatoes.py to create an html file for display
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -1,15 +1,19 @@
 import webbrowser
 
+
 # This class provides a way to store movie related information.
 class Movie():
-    
+
     # Initialize this class with the variables you'll be passed.
-    def __init__(self,movie_title, movie_storyline,poster_image,trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-    
-    # Using the library webbrowser - open a new window and play the YouTube trailer.
+
+    '''Using the library webbrowser - open a new window and play the YouTube
+        trailer.'''
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
